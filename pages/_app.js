799efb190ce8d45fcb03be89ea5a0e8474a8086f
prains <sleskov2013@yandex.ru>
2023-04-14 22:21:52 +0300

    add margin auto to layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className="w-[87%]">
+    <main className="w-[87%] my-0 mx-auto">
       <Component {...pageProps} />
     </main>
   );
