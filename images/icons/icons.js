0bea10ae7e02dc41618749125e28089538e44464
prains <sleskov2013@yandex.ru>
2023-04-14 22:14:51 +0300

    add burger from icons

--- images/icons/icons.js
+++ images/icons/icons.js
@@ -2,5 +2,6 @@ import instagramm from "./Frame (4).svg";
 import facebook from "./Frame (3).svg";
 import linkendin from "./Frame (2).svg";
 import youtube from "./Frame (1).svg";
+import burger from "./burger.svg";
 
-export { instagramm, facebook, linkendin, youtube };
+export { instagramm, facebook, linkendin, youtube, burger };
