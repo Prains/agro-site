acd6140605dd44f109fa185bbfb59a217ac20a8c
prains <sleskov2013@yandex.ru>
2023-04-14 22:21:26 +0300

    add content sizing at component layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import "@/styles/globals.css";
 
-
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className="w-[87%]">
+      <Component {...pageProps} />
+    </main>
+  );
 }
