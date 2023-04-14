d0989e55091232a2c486e0835010fa28b2de06db
prains <sleskov2013@yandex.ru>
2023-04-14 22:31:50 +0300

    add flex centrilizing to app js

---
+++ components/Main/Main.jsx
@@ -0,0 +1,9 @@
+import Title from "../ui/Title/Title";
+
+const Main = () => {
+  return <section>
+    <Title>ПОСТАВЩИК УКРЫВНЫХ МАТЕРИАЛОВ</Title>
+  </section>;
+};
+
+export default Main;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-import Header from "@/components/Header/Header";
-import Button from "@/components/ui/Button/Button";
-import logo from "@/images/logo/logo";
-import Image from "next/image";
+
 
 export default function Home() {
-  return <></>;
+  return ;
 }
