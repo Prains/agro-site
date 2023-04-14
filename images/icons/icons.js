d31b38d2afb36262fd517b936d79bbd09b024874
prains <sleskov2013@yandex.ru>
2023-04-14 22:12:27 +0300

    add icons

---
+++ images/icons/icons.js
@@ -0,0 +1,6 @@
+import instagramm from "./Frame (4).svg";
+import facebook from "./Frame (3).svg";
+import linkendin from "./Frame (2).svg";
+import youtube from "./Frame (1).svg";
+
+export { instagramm, facebook, linkendin, youtube };
