4679dc85c8f9b9f0e331a795af22cd0d8887d566
prains <sleskov2013@yandex.ru>
2023-04-15 21:47:26 +0300

    change button height

--- components/Mainpage/Partner/PartnerForm/ButtonsList/ButtonsList.jsx
+++ components/Mainpage/Partner/PartnerForm/ButtonsList/ButtonsList.jsx
@@ -1,7 +1,7 @@
 const ButtonList = () => {
   return (
     <div className="flex items-center justify-between w-full mt-[64px] max-w-[617px]">
-      <button className="bg-transparent border-[#535356] border border-solid max-w-[280px] w-full h-[35px] text-[#7E7F81] font-text text-xs">
+      <button className="bg-transparent border-[#535356] border border-solid max-w-[280px] w-full h-[35px] md:h-[46px] text-[#7E7F81] font-text text-xs">
         Скачать предложение
       </button>
       <button
