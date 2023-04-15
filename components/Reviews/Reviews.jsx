4cb99d244dc8f5a5844eb595502533266a2648d0
prains <sleskov2013@yandex.ru>
2023-04-15 19:02:02 +0300

    add reviews jsx

---
+++ components/Reviews/Reviews.jsx
@@ -0,0 +1,10 @@
+import { register } from "swiper/element/bundle";
+
+const Reviews = () => {
+  register();
+  return <section className="w-[87%]">
+    
+  </section>;
+};
+
+export default Reviews;
