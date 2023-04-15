a4bf0e8168e6239e29aa37fb5b5b2ce3acd708f7
prains <sleskov2013@yandex.ru>
2023-04-15 22:24:21 +0300

    fix bottom list item

--- components/Mainpage/Main/BottomList/BottomListItem/BottomListItem.jsx
+++ components/Mainpage/Main/BottomList/BottomListItem/BottomListItem.jsx
@@ -3,10 +3,10 @@ import { Paragraph } from "@/components/ui";
 const BottomListItem = ({ item }) => {
   return (
     <div
-      className={`w-[25%] ${item.border} border-solid border-[#C8C8C8] flex items-center justify-center flex-col py-[21px]`}
+      className={`w-[25%] ${item.border} border-solid border-[#C8C8C8] flex items-center justify-center flex-col`}
     >
       <div className="flex items-start flex-col justify-center">
-        <Paragraph className="lg:text-[11px]">{item.title}</Paragraph>
+        <Paragraph className="md:text-[11px]">{item.title}</Paragraph>
         <h2 className={"font-title font-bold text-3xl lg:text-[32px] text-textBase"}>{item.subtitle}</h2>
       </div>
     </div>
