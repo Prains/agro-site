caf44c2ae2b0fb72db4ab63b8ae0887c7c5d7114
prains <sleskov2013@yandex.ru>
2023-04-15 21:52:11 +0300

    fix partner font size

--- components/Mainpage/Partner/ContactBlock/ContactHeader/ContactHeader.jsx
+++ components/Mainpage/Partner/ContactBlock/ContactHeader/ContactHeader.jsx
@@ -2,7 +2,7 @@ import { Subtitle } from "@/components/ui";
 const ContactHeader = () => {
   return (
     <>
-      <Subtitle className="text-white mb-[4px] lg:mb-[40px]">Контакты</Subtitle>
+      <Subtitle className="text-white mb-[4px] lg:mb-[40px] lg:text-4xl">Контакты</Subtitle>
       <p className="font-text text-[#898989] text-lg md:text-xl mb-7 xl:w-7/12">
         Cвяжитесь с нами и получите скидку на первый заказ
       </p>
--- components/Mainpage/Partner/PartnerForm/PartnerForm.jsx
+++ components/Mainpage/Partner/PartnerForm/PartnerForm.jsx
@@ -5,7 +5,7 @@ import ButtonList from "./ButtonsList/ButtonsList";
 const PartnerForm = () => {
   return (
     <form className="flex items-center justify-center flex-col lg:pr-[62px] lg:w-2/3">
-      <Subtitle className="text-white text-xl mb-[58px] md:text-3xl">
+      <Subtitle className="text-white text-xl mb-[58px] lg:text-3xl">
         Станьте нашим партнером - получите индивидуальное коммерческое
         предложение
       </Subtitle>
