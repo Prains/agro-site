deae831e671566e6ab72d1079174a4ba4a1a68fe
prains <sleskov2013@yandex.ru>
2023-04-15 22:26:54 +0300

    fix text bottomlist

--- components/Mainpage/Main/BottomList/BottomListItem/BottomListItem.jsx
+++ components/Mainpage/Main/BottomList/BottomListItem/BottomListItem.jsx
@@ -6,8 +6,16 @@ const BottomListItem = ({ item }) => {
       className={`w-[25%] ${item.border} border-solid border-[#C8C8C8] flex items-center justify-center flex-col`}
     >
       <div className="flex items-start flex-col justify-center">
-        <Paragraph className="md:text-[11px]">{item.title}</Paragraph>
-        <h2 className={"font-title font-bold text-3xl lg:text-[32px] text-textBase"}>{item.subtitle}</h2>
+        <p className={"font-text text-textBase text-lg md:text-[11px]"}>
+          {item.title}
+        </p>
+        <h2
+          className={
+            "font-title font-bold text-3xl lg:text-[32px] text-textBase"
+          }
+        >
+          {item.subtitle}
+        </h2>
       </div>
     </div>
   );
